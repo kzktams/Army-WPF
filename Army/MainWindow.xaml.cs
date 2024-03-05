5d5a9b03c4b1381fc38fef2097c1719b5c74cd7d
Bartapeter <lombotom17@gmail.com>
2024-03-05 11:21:48 +0100

    Changes

--- Army/MainWindow.xaml.cs
+++ Army/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,10 +18,42 @@ namespace Army
 {
     public partial class MainWindow : Window
     {
+        ObservableCollection<Trooper> army;
+        ObservableCollection<Trooper> all = new ObservableCollection<Trooper>();
+        int allCost = 0;
         public MainWindow()
         {
             InitializeComponent();
+            ObservableCollection<Trooper> army = new ObservableCollection<Trooper>()
+            {
+                new Trooper(){Name = "pilot", Power=3, Vitality = 10},
+                new Trooper(){Name = "marine", Power=10, Vitality = 7},
+                new Trooper(){Name = "sniper", Power=9, Vitality = 6},
+                new Trooper(){Name = "engineer", Power=5, Vitality = 6}
+            };
+            lBox.ItemsSource = army;
+            lBox2.ItemsSource = all;
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            all.Add((Trooper)lBox.SelectedItem);
+            foreach (var item in all)
+            {
+                allCost += item.Cost;
+            }
+            lb_cost.Content = allCost.ToString() + "$";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            all.Remove((Trooper)lBox2.SelectedItem);
+            allCost = 0;
+            foreach (var item in all)
+            {
+                allCost += item.Cost;
+            }
+            lb_cost.Content = allCost.ToString() + "$";
         }
     }
 }
